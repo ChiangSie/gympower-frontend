d06d672c229ef1d11b92ebbe8b39d83927a7aa46
youk2333 <youk2333@gmail.com>
2024-05-28 11:42:19 +0800

    add view

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <nav>
-      <RouterLink to="/Aboutus">關於我們</RouterLink>
-      <!-- <RouterLink to="/News">最新活動</RouterLink>
-        <RouterLink to="/Diary">健身日記</RouterLink>
-        <RouterLink to="/Course">課程資訊</RouterLink>
-        <RouterLink to="/Bento">美味食堂</RouterLink>
-        <RouterLink to="/Coach">教練介紹</RouterLink>
-        <RouterLink to="/Login">會員登入</RouterLink> -->
+      <RouterLink to="/AboutusView">關於我們</RouterLink>
+      <RouterLink to="/NewsView">最新活動</RouterLink>
+      <RouterLink to="/DiaryView">健身日記</RouterLink>
+      <RouterLink to="/CourseView">課程資訊</RouterLink>
+      <RouterLink to="/BentoView">美味食堂</RouterLink>
+      <RouterLink to="/CoachView">教練介紹</RouterLink>
+      <RouterLink to="/LoginView">會員登入</RouterLink>
     </nav>
   </header>
 
@@ -23,26 +23,11 @@ import HelloWorld from './components/HelloWorld.vue'
 <script>
 import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-// import Aboutus from '@/views/Aboutus.vue'
-// import News from '@/views/News.vue'
-// import Diary from '@/views/Diary.vue'
-// import Course from '@/views/Course.vue'
-// import Bento from '@/views/Bento.vue'
-// import Coach from '@/views/Coach.vue'
-// import Login from '@/views/Login.vue'
 
 export default defineComponent({
   components: {
     RouterLink,
     RouterView
-    // Aboutus,
-    // News,
-    // Diary,
-    // Course,
-    // Bento,
-    // Coach,
-    // Login
-    // 哈
   }
 })
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,42 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/Aboutus',
-      name: 'aboutus',
+      path: '/AboutusView',
+      name: 'AboutusView',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutusView.vue')
+    },
+    {
+      path: '/BentoView',
+      name: 'BentoView',
+      component: () => import('../views/BentoView.vue')
+    },
+    {
+      path: '/CoachView',
+      name: 'CoachView',
+      component: () => import('../views/CoachView.vue')
+    },
+    {
+      path: '/CourseView',
+      name: 'CourseView',
+      component: () => import('../views/CourseView.vue')
+    },
+    {
+      path: '/DiaryView',
+      name: 'DiaryView',
+      component: () => import('../views/DiaryView.vue')
+    },
+    {
+      path: '/LoginView',
+      name: 'LoginView',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/NewsView',
+      name: 'NewsView',
+      component: () => import('../views/NewsView.vue')
     }
   ]
 })
