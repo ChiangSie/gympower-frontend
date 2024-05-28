e46198c18b8c923955dfd1e18452f39ccce6c3da
Sam <a22128588@gmail.com>
2024-05-28 19:07:16 +0800

    首頁加上banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,15 @@
 <template>
+  <section class="section section-banner">
+    <div class="container container-banner">
+      <div class="row">
+        <div class="col-12 col-md-12 col-lg-12 col-xl-12">
+          <picture>
+            <img src="https://picsum.photos/300/200/?random=10">
+          </picture>
+        </div>
+      </div>
+    </div>
+  </section>
   <section>
     <div class="container">
       <div class="row">
@@ -74,60 +85,7 @@
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
-          <div class="card">
-            <div class="pic"><img src="https://picsum.photos/300/200/?random=10"></div>
-            <div class="txt">
-              <h3>title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus quis voluptatibus a!</p>
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
   </section>
@@ -135,8 +93,8 @@
 
 
 <style scoped>
-  img {
-    width: 100%;
-    vertical-align: top;
-  }
+img {
+  width: 100%;
+  vertical-align: top;
+}
 </style>
