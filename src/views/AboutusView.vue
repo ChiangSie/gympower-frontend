1e2b36811d89eba17573760cbaccf8bb66fa5f9f
ChiangSie <jimmy7369699@gmail.com>
2024-05-31 16:32:31 +0800

    aboutus

--- src/views/AboutusView.vue
+++ src/views/AboutusView.vue
@@ -5,6 +5,18 @@
     </div>
     <h1>關於我們</h1>
   </section>
+
+  <section class="slogan">
+    <p>
+      GYM 力的誕生源於我們對健康和運動的熱愛，以及對改善人們生活質量的強烈願望。
+      在創始人親身經歷了通過健身改變生活的過程後，他們深刻認識到健身對於提升體能、強化心理素質以及增進整體健康的重要性。
+      因此，GYM
+      力的創立目標是為社區提供一個專業、友好且充滿激勵的健身環境，讓每個人都能找到屬於自己的健身方式。
+    </p>
+    <div class="slogan_img">
+      <img src="~@/assets/img/aboutus_slogan.jpg" alt="" />
+    </div>
+  </section>
 </template>
 
 <script></script>
@@ -14,7 +26,7 @@ section {
   display: flex;
   justify-content: flex-end;
   width: 100%;
-  padding: 3% 10%;
+  padding: 3% 14%;
   position: relative;
   .banner_aboutus_img {
     width: 60%;
@@ -31,6 +43,28 @@ section {
     position: absolute;
     top: 50%;
     left: 20%;
+  }
+}
+
+.slogan {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  gap: 15px;
+  p {
+    width: calc(45% - 15px);
+    margin-left: 5%;
+    padding-top: 10%;
+  }
+  .slogan_img {
+    width: 30%;
+    aspect-ratio: 1/1;
+    img {
+      width: 100%;
+      aspect-ratio: 1/1;
+      object-fit: cover;
+      object-position: 50% 50%;
+    }
   }
 }
 </style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -5,18 +5,6 @@
     </div>
     <h1>最新消息</h1>
   </section>
-
-  <section class="slogan">
-    <p>
-      GYM 力的誕生源於我們對健康和運動的熱愛，以及對改善人們生活質量的強烈願望。
-      在創始人親身經歷了通過健身改變生活的過程後，他們深刻認識到健身對於提升體能、強化心理素質以及增進整體健康的重要性。
-      因此，GYM
-      力的創立目標是為社區提供一個專業、友好且充滿激勵的健身環境，讓每個人都能找到屬於自己的健身方式。
-    </p>
-    <div class="slogan_img">
-      <img src="~@/assets/img/aboutus_slogan.jpg" alt="" />
-    </div>
-  </section>
 </template>
 
 <script></script>
