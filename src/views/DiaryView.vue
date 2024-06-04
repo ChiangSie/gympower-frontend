8bd6ed5513e17209f6a17e7d61b1980b3f48b7f8
Sam <a22128588@gmail.com>
2024-06-05 03:20:04 +0800

    首頁新增bento-section

--- src/views/DiaryView.vue
+++ src/views/DiaryView.vue
@@ -4,33 +4,33 @@
   <div class="section section-search">
     <div class="container container-search">
       <div class="search">
-      <p>健身交換日記</p>
-      <input class="search-input" type="text">
-      <button class="search-btn">+ 新增</button>
-      </div>  
-      <div class="page"><a href=""  v-for="n in 5">{{n}}</a></div>
-  </div>
-  <div class="section section-diary">
-<div class="container container-diary">
-  <div class="row">
-     <div class="col-12 col-md-6 col-lg-4 col-xl-3" v-for="card in cardlist" :key="card.id">
-          <div class="card">
-            <div class="pic">
-              <img :src="card.imgSrc" />
-              <div class="box"></div>
-               <div class="title">
-              <h3>{{ card.title }}</h3>
-            </div>
-            </div>
-            <div class="txt">
-              <p>{{ card.text }}</p>
+        <p>健身交換日記</p>
+        <input class="search-input" type="text">
+        <button class="search-btn">+ 新增</button>
+      </div>
+      <div class="page"><a href="" v-for="n in 5">{{ n }}</a></div>
+    </div>
+    <div class="section section-diary">
+      <div class="container container-diary">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-4 col-xl-3" v-for="card in cardlist" :key="card.id">
+            <div class="card">
+              <div class="pic">
+                <img :src="card.imgSrc" />
+                <div class="box"></div>
+                <div class="title">
+                  <h3>{{ card.title }}</h3>
+                </div>
+              </div>
+              <div class="txt">
+                <p>{{ card.text }}</p>
+              </div>
             </div>
           </div>
         </div>
-  </div>
-</div>
+      </div>
 
-  </div>
+    </div>
   </div>
 
 
@@ -82,85 +82,99 @@ export default {
 }
 </script>
 <style lang="scss">
-body{
+body {
   background-color: #F9F8F7;
 }
-img{
+
+img {
   width: 100%;
 }
-a{
+
+a {
   text-decoration: none;
   color: #000000;
 }
+
 .container {
   width: 100%;
   max-width: 1200px;
   padding: .3rem 0;
 }
-.container-search{
+
+.container-search {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   flex-wrap: wrap;
-  .search{
-  display: flex;
-  align-items: center;
-  gap:10px;
-  .search-input{
-  padding:10px 10px 10px 35px;
-  border : solid .1px #b4def2;
-  color:#fff;
-  border-radius: 35px;
-  background-color :  #b4def2;
-  outline:none;  
+
+  .search {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .search-input {
+      padding: 10px 10px 10px 35px;
+      border: solid .1px #b4def2;
+      color: #fff;
+      border-radius: 35px;
+      background-color: #b4def2;
+      outline: none;
+    }
+
+    .search-btn {
+      padding: 10px 15px;
+      border: solid .1px #ffffff;
+      background-color: #002451;
+      color: #fff;
+      border-radius: 35px;
+    }
   }
-  .search-btn{
-    padding: 10px 15px;
-    border : solid .1px #ffffff;
-    background-color: #002451;
-    color: #fff;
-    border-radius: 35px;
-  }
-  }
+
   .page {
     align-self: center;
-  a{
-    margin:0 .3rem;
-    padding: .3rem .5rem;
-    background-color: #002451;
-    color: #fff;
-    border-radius: 50%;
-    align-self: center;
-  }}
-}
-.container-diary{
-.card{
-  .pic{
-    position: relative;
-    border-radius: 10px;
-    img{
-    border-radius: 10px;
-    }
-    .title{
-    position: absolute;
-    width: 200px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    z-index: 3;
-    }
-    .box{
-    position: absolute;
-    width: 100%;
-    height: 150px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #fffeee9d;
+
+    a {
+      margin: 0 .3rem;
+      padding: .3rem .5rem;
+      background-color: #002451;
+      color: #fff;
+      border-radius: 50%;
+      align-self: center;
     }
   }
-  
 }
+
+.container-diary {
+  .card {
+    .pic {
+      position: relative;
+      border-radius: 10px;
+
+      img {
+        border-radius: 10px;
+      }
+
+      .title {
+        position: absolute;
+        width: 200px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        z-index: 3;
+      }
+
+      .box {
+        position: absolute;
+        width: 100%;
+        height: 150px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fffeee9d;
+      }
+    }
+
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -50,7 +50,6 @@
   <section class="section section-classinfo">
     <div class="container container-classinfo">
       <!-- 背景裝飾字 -->
-
       <h2 class="decorate-txt">COURSES</h2>
       <h2 class="title">課程資訊</h2>
       <div class="row">
@@ -100,7 +99,86 @@
       </div>
     </div>
   </section>
+  <!-- 餐點 -->
+  <section class="section section-bento">
+    <div class="container container-bento">
+      <h2 class="decorate-txt flex-end">BENTO</h2>
+      <h2 class="title">客製化專屬於你的餐盒</h2>
+      <div class="row">
+        <div class="col-6">
+          <div class="left">
+            <div class="customized">
+              <h3>客製餐盒</h3>
+              <p>會員專屬客製化餐盒，為你的健康而設計。依據你的運動目標和營養需求精心搭配， 無論是增肌、減脂，還是保持健康體型，我們的餐點都能助你達成健身目標。健康從飲食開始，讓我們陪伴你走向更強壯、更健康的未來。</p>
+              <button>開始客製</button>
+              <div class="pic-bento">
+                <img src="../assets/img/pic-bento.png">
+              </div>
+            </div>
 
+          </div>
+        </div>
+        <div class=" col-right col-6">
+          <div class="right">
+            <div class="choose">
+              <div class="txt">
+                <h4>選擇餐盒大小</h4>
+                <p>
+                  提供四格和五格兩種尺寸，靈活滿足你的營養需求
+                </p>
+              </div>
+              <div class="pic">
+                <img src="https://picsum.photos/300/200/?random=10">
+              </div>
+            </div>
+            <div class="choose">
+              <div class="pic">
+                <img src="https://picsum.photos/300/200/?random=10">
+              </div>
+              <div class="txt">
+                <h4>選擇喜愛菜色</h4>
+                <p>
+                  豐富多樣的菜色選擇，顛覆您對健康餐的味蕾想像
+                </p>
+              </div>
+            </div>
+            <div class="choose">
+              <div class="txt">
+                <h4>訂製營養熱量</h4>
+                <p>
+                  清晰標示食材的熱量與營養成分，精準符合您的營養所需
+                </p>
+              </div>
+              <div class="pic">
+                <img src="https://picsum.photos/300/200/?random=10">
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+  <!-- 教練 -->
+  <section class="section section-coach">
+    <div class="container container-coach">
+      <h2 class="decorate-txt">OUR COACH</h2>
+    </div>
+  </section>
+  <!-- 學員回饋 -->
+  <section class="section section-feedback">
+    <div class="container container-feedback">
+      <h2 class="decorate-txt flex-end">FEEDBACK</h2>
+    </div>
+  </section>
+  <!-- 扭蛋 -->
+  <section class="section section-coupon">
+    <div class="container container-coupon">
+      <h2 class="decorate-txt ">COUPON</h2>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -164,12 +242,33 @@ export default {
 
 <!-- lang="sass" -->
 <style lang="scss" scoped>
+// 背景裝飾文字
+.decorate-txt {
+  -webkit-text-stroke: .8px #7E7E7E;
+  color: #F9F8F7;
+  display: flex;
+  font-size: clamp(60px, 6.25vw, 80px);
+  letter-spacing: 0;
+  font-weight: bold;
+  justify-content: flex-start;
+}
+
+.flex-end {
+  justify-content: flex-end;
+}
+
+// 首頁 BANNER
 .section-banner {
+  margin-top: 38.42px;
+
   .container-banner {
+
+    max-width: unset;
+
     .banner {
       background-image: url('../assets/img/index-banner.png');
       background-size: 100% 100%;
-      border-radius: 10px;
+      // border-radius: 10px;
 
 
       .banner-txt {
@@ -177,7 +276,7 @@ export default {
         background-color: rgba($color: #fff, $alpha: .6);
         border-radius: 10px;
         width: fit-content;
-        padding: 5% 5% 5% 1%;
+        padding: 5% 5% 5% 2%;
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -185,7 +284,7 @@ export default {
 
 
         h2 {
-          font-size: clamp(20px, 4vw, 40px);
+          font-size: clamp(20px, 3.125vw, 40px);
 
           .en {
             color: #fff;
@@ -214,18 +313,8 @@ export default {
   }
 }
 
+// 課程資訊
 .container-classinfo {
-  .decorate-txt {
-    // 背景裝飾文字
-    -webkit-text-stroke: .8px #7E7E7E;
-    color: #F9F8F7;
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-end;
-    font-size: clamp(60px, 6.25vw, 80px);
-    letter-spacing: 0;
-    font-weight: bold;
-  }
 
   .title {
     text-align: center;
@@ -240,8 +329,6 @@ export default {
   }
 
   .row {
-
-
     .card {
       margin-bottom: 30px;
       background-color: #fff;
@@ -323,6 +410,114 @@ export default {
       margin-left: auto;
       margin-right: 15px;
     }
+  }
+}
+
+// 客製餐點
+.section-bento {
+  .container-bento {
+    max-width: 1100px;
+    // display: flex;
+
+    .title {
+      font-weight: bold;
+      text-align: center;
+      margin-bottom: 60px;
+    }
+
+    .row {
+      background-color: #fff;
+      border-radius: 10px;
+      padding: 2% 1%;
+      align-items: center;
+      flex-wrap: nowrap;
+
+      justify-content: space-between;
+
+      .col-right {
+        width: 40%;
+      }
+
+      .left {
+        .customized {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          gap: 40px;
+
+          h3 {
+            font-weight: bold;
+          }
+
+          button {
+            line-height: 30px;
+            width: clamp(100px, 30%, 150px);
+            height: 50px;
+            border: none;
+            background: #002451;
+            border-radius: 10px;
+            color: #fff;
+            font-size: clamp(16px, 1.5625vw, 20px);
+          }
+
+          .pic-bento {
+            width: 100%;
+            text-align: center;
+            background-color: #D4EAF7;
+            border-radius: 10px;
+            // margin-left: -30px;
+
+            img {
+              vertical-align: middle;
+            }
+          }
+
+
+
+        }
+      }
+
+      .right {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: clamp(70px, 3.125vw, 40px);
+
+
+        .choose {
+          display: flex;
+          align-items: center;
+          gap: 5%;
+          // justify-content: space-around;
+
+          // justify-content: flex-end;
+
+          .txt {
+            h4 {
+              font-weight: bold;
+            }
+          }
+
+          .pic {
+            flex-shrink: 0;
+            width: 30%;
+            // height: 20%;
+            border-radius: 50%;
+            overflow: hidden;
+
+
+            img {
+              display: block;
+              aspect-ratio: 1 / 1;
+              width: 100%;
+            }
+          }
+        }
+      }
+    }
+
   }
 }
 </style>
