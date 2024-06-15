918767b5edf34a6ba9221e7a2aa7b341e6d69273
funkyHCIguru <krystalcheng0421@gmail.com>
2024-06-15 17:05:44 +0800

    便當做篩選

--- src/views/Bento/BentoPage2/BentoCate.vue
+++ src/views/Bento/BentoPage2/BentoCate.vue
@@ -1,43 +1,71 @@
 <template>
     <section class="category">
-        <div class="button" v-for="btn in option" :key="btn.btn">
-            <div>
-                <a href="#" class="button_link">{{ btn.btn }}
-                </a>
-            </div>
+        <div class="button_list">
+            <button class="btn_tag" @click="filter_tag('')">全部</button>
+            <button class="btn_tag" @click="filter_tag('主食')">主食</button>
+            <button class="btn_tag" @click="filter_tag('肉類')">肉類</button>
+            <button class="btn_tag" @click="filter_tag('蔬菜類')">蔬菜類</button>
+            <button class="btn_tag" @click="filter_tag('海鮮類')">海鮮類</button>
+            <button class="btn_tag" @click="filter_tag('蛋豆類')">蛋豆類</button>
+        </div>
+        <div v-if="sourceData.length === 0">Loading...</div>
+        <div v-else>
+            <!-- <div v-for="food in sourceData" :key="food.id" :food="food">{{ sourceData }}</div> -->
+            <!-- <BentoFood >
+                {{sourceData }}
+            </BentoFood> -->
+            <BentoFood :foods="filterList" />
         </div>
     </section>
 </template>
 
 <script>
+import BentoFood from './BentoFood.vue'
+
 export default {
+    components: {
+        BentoFood
+    },
     data() {
         return {
-            option: [
-                {
-                    btn: '主食'
-                },
-                {
-                    btn: '炸物'
-                },
-                {
-                    btn: '肉類'
-                },
-                {
-                    btn: '蔬菜類'
-                },
-                {
-                    btn: '海鮮類'
-                },
-                {
-                    btn: '蛋豆類'
-                }
-            ]
+            sourceData: [],
+            currentTag: '',
+            foods: [{}]
+        }
+    },
+    mounted() {
+        fetch('/json/bento.json')
+            .then((res) => res.json())
+            .then((json) => {
+                //確認有沒有response
+                console.log(json)
+                //顯示用
+                this.sourceData = json
+            })
+    },
+    computed: {
+        filterList() {
+            if (this.currentTag == '') return this.sourceData
+            return this.sourceData.filter((food) => {
+                return food.tag == this.currentTag
+            })
+        }
+    },
+    methods: {
+        clear() {
+            //     this.sourceData = this.foods;
+        },
+        filter_tag(tagName) {
+            this.currentTag = tagName
+            // this.sourceData = this.foods.filter(food => {
+            //     return food.tag === tagName;
+            // });
         }
     }
-};
+}
 </script>
-<style>
+
+<style lang="scss" scoped>
 * {
     text-decoration: none;
 }
@@ -53,25 +81,26 @@ export default {
     }
 }
 
-.button {
+.button_list {
     display: inline-block;
     margin: 0 8px 0;
 }
 
-.button_link {
+.btn_tag {
     display: inline-block;
-    margin-top: 16px;
+    margin: 16px 16px;
     padding: 6px 16px;
-    border: 3px solid #71C4EF;
-    background-color: #F9F8F7;
-    color: #71C4EF;
+    border: 3px solid #71c4ef;
+    background-color: #f9f8f7;
+    color: #71c4ef;
     width: 120px;
     height: 40px;
     border-radius: 30px;
 }
 
-.button_link:hover {
-    background-color: #71C4EF;
+.btn_tag:hover {
+    background-color: #71c4ef;
     color: #ffffff;
+    cursor: pointer;
 }
 </style>
