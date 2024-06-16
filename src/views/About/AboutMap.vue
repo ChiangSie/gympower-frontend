41d7658ff3f640ee0b0bf1a17025c0aab3e3cf9a
MinnaYeh <minna10472@gmail.com>
2024-06-16 18:47:22 +0800

    修改map svg

--- src/views/About/AboutMap.vue
+++ src/views/About/AboutMap.vue
@@ -17,13 +17,13 @@
 import axios from 'axios'
 
 export default {
-  name: 'App',
-  data() {
+name: 'App',
+data() {
     return {
-      now_area: null,
-      svgContent: '',
-      filter: '',
-      placeData: [
+    now_area: null,
+    svgContent: '',
+    filter: '',
+    placeData: [
         { tag: 'taipei_city', place: '臺北市', low: 20, high: 24, weather: 'Sunny' },
         { tag: 'new_taipei_city', place: '新北市', low: 20, high: 24, weather: 'Sunny' },
         { tag: 'taichung_city', place: '台中市', low: 20, high: 24, weather: 'Sunny' },
@@ -46,100 +46,100 @@ export default {
         { tag: 'penghu_country', place: '澎湖縣', low: 20, high: 24, weather: 'Sunny' },
         { tag: 'kinmen_country', place: '金門縣', low: 20, high: 24, weather: 'Sunny' },
         { tag: 'lienchiang_country', place: '連江縣', low: 20, high: 24, weather: 'Sunny' }
-      ]
+    ]
     }
-  },
-  computed: {
+},
+computed: {
     now_area() {
-      const result = this.placeData.filter((obj) => obj.tag === this.filter)
-      return result.length > 0 ? result[0] : null
+    const result = this.placeData.filter((obj) => obj.tag === this.filter)
+    return result.length > 0 ? result[0] : null
     }
-  },
-  methods: {
+},
+methods: {
     fetchSvg() {
-      axios
+    axios
         .get('/src/assets/img/taiwan_map.svg')
         .then((response) => {
-          this.svgContent = response.data
-          this.$nextTick(() => {
+        this.svgContent = response.data
+        this.$nextTick(() => {
             this.addMouseEventListeners()
-          })
+        })
         })
         .catch((error) => {
-          console.error('Error fetching SVG:', error)
+        console.error('Error fetching SVG:', error)
         })
     },
     addMouseEventListeners() {
-      const svgPaths = this.$el.querySelectorAll('path')
-      svgPaths.forEach((path) => {
+    const svgPaths = this.$el.querySelectorAll('path')
+    svgPaths.forEach((path) => {
         path.addEventListener('mouseenter', this.handleMouseEnter)
-      })
+    })
     },
     handleMouseEnter(event) {
-      const tagname = event.target.getAttribute('data-name')
-      if (tagname) {
+    const tagname = event.target.getAttribute('data-name')
+    if (tagname) {
         this.filter = tagname
-      }
     }
-  },
-  mounted() {
+    }
+},
+mounted() {
     this.fetchSvg()
-  }
+}
 }
 </script>
 
 <style>
 * {
-  font-family: 'Fjalla One';
+font-family: 'Fjalla One';
 }
 
 svg {
-  height: 100vh;
+height: 100vh;
 }
 
 body {
-  background-color: rgba(34, 34, 34, 0.9);
+background-color: rgba(34, 34, 34, 0.9);
 }
 
 path {
-  stroke: white;
-  fill: transparent;
-  transition: 0.5s;
-  cursor: pointer;
+stroke: white;
+fill: transparent;
+transition: 0.5s;
+cursor: pointer;
 }
 
 path:hover {
-  fill: #b99362;
-  transform: translate(-5px, -5px);
+fill: #b99362;
+transform: translate(-5px, -5px);
 }
 
 .title_area {
-  position: absolute;
-  right: 20px;
-  top: 20px;
-  color: white;
-  font-size: 25px;
+position: absolute;
+right: 20px;
+top: 20px;
+color: white;
+font-size: 25px;
 }
 
 hr {
-  width: 100px;
-  margin-left: 0;
-  border: none;
-  border-bottom: solid 2px #b99362;
+width: 100px;
+margin-left: 0;
+border: none;
+border-bottom: solid 2px #b99362;
 }
 
 h1 {
-  margin: 0;
-  padding: 0;
+margin: 0;
+padding: 0;
 }
 
 .forcast h2,
 .forcast h4,
 .forcast h5 {
-  margin: 0;
+margin: 0;
 }
 
 .forcast h2 {
-  margin-top: 50px;
+margin-top: 50px;
 }
 </style>
