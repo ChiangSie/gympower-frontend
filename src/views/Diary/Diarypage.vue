79985f4b8cb6958ad6485f96c981fca916b91058
Sam <a22128588@gmail.com>
2024-06-25 20:10:23 +0800

    內頁大致完成，剩下內容部分再請GPT生出來

--- src/views/Diary/Diarypage.vue
+++ src/views/Diary/Diarypage.vue
@@ -1,9 +1,48 @@
 <template>
   <Banner :title="'健身日記'" />
-  <div class="section section-author">
-    {{ diaryContent.id }}
+  <div class="section section-article">
+    <div class="container">
+      <div class="date">
+        <p>
+          {{ diaryContent.date }}
+        </p>
+      </div>
+      <div class="author">
+        <p>{{ diaryContent.author }}</p>
+      </div>
+      <div class="title">
+        <div class="text">
+          <p>{{ diaryContent.title }}</p>
+        </div>
+        <div class="movement">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          <i class="fa-regular fa-heart"></i>
+          <i class="fa-regular fa-thumbs-up"></i>
+        </div>
+      </div>
+      <div class="line"></div>
+      <div class="subtitle">
+        <p>
+          {{ diaryContent.subtitle }}
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-6 col-md-6 col-lg-6 col-xl-6">
+          <div class="content">
+            <p>
+              {{ diaryContent.text }}
+            </p>
+          </div>
+        </div>
+        <div class="col-6 col-md-6 col-lg-6 col-xl-6">
+          <div class="pic">
+            <img :src="parseImg(diaryContent.imgSrc)" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="line"></div>
+    </div>
   </div>
-  <div class="section section-article"></div>
 </template>
 
 <script>
@@ -36,12 +75,48 @@ export default {
         });
       });
   },
+  methods: {
+    parseImg(imgURL) {
+      return new URL(`../../assets/img/Diary/${imgURL}`, import.meta.url).href;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-p {
-  width: 500px;
-  background-color: #ff0000;
+.container {
+  max-width: 900px;
+  .author {
+    margin: 5px 0;
+    font-weight: bold;
+  }
+  .title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .text {
+      font-weight: bold;
+    }
+    .movement {
+      display: flex;
+      gap: 10px;
+    }
+  }
+  .subtitle {
+    width: 50%;
+    margin: 10px 10px;
+  }
+  .line {
+    border-bottom: 1px solid #000;
+    margin: 10px 0;
+  }
+  .row {
+    .pic {
+      img {
+        width: 100%;
+        border-radius: 10px;
+      }
+    }
+  }
 }
 </style>
