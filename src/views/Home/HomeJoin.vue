7eb413e0def444ceec2e9fdb0452b366b3e1a128
MinnaYeh <minna10472@gmail.com>
2024-06-29 16:10:06 +0800

    add link連結& 圖片

--- src/views/Home/HomeJoin.vue
+++ src/views/Home/HomeJoin.vue
@@ -6,7 +6,10 @@
     <p>
       無論你是健身新手還是經驗豐富的健身愛好者，我們的專業教練團隊將為您量身定制專屬健身計劃，幫助你達到最佳狀態，並且提供每日新鮮的健康餐盒，讓你在健身的同時享受美味、營養均衡的餐食。告別高熱量便當，每一餐都是最健康的選擇！加入課程即可抽取優惠券，享受更多驚喜折扣和福利。每次參加課程都是一次贏得優惠的機會，讓你的健身之旅充滿期待!
     </p>
-    <button><a href="#" class="cta">加入GYM力</a></button>
+
+    <router-link to="/login">
+      <button><span class="cta">加入GYM力</span></button>
+    </router-link>
   </section>
 </template>
 
